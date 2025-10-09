813ee1385720ec96380d5b02283f1bd657fae334
Elvin Mendez <[elvinmanuel005@gmail.com]>
2025-10-08 23:58:13 -0400

    useEffect

--- hook-app/src/main.tsx
+++ hook-app/src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import { HookApp } from './HookApp'
+// import { TrafficLight } from './01- useState/TrafficLight'
+import { TrafficLightWithEffect } from './02- useEffect/TrafficLightWithEffect'
 
 import './index.css'
-import { TrafficLight } from './01- useState/TrafficLight'
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
      {/* <HookApp /> */}
-     <TrafficLight />
+     {/* <TrafficLight /> */}
+     <TrafficLightWithEffect />
   </StrictMode>,
 )
