ece7d7b5f9059693cd9c8f05aa49255cf8d4b2e9
Elvin Mendez <[elvinmanuel005@gmail.com]>
2025-10-10 22:27:36 -0400

    useRef and others hooks

--- hook-app/src/main.tsx
+++ hook-app/src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import { HookApp } from './HookApp'
 // import { TrafficLight } from './01- useState/TrafficLight'
-import { TrafficLightWithEffect } from './02- useEffect/TrafficLightWithEffect'
+// import { TrafficLightWithEffect } from './02- useEffect/TrafficLightWithEffect'
+// import { TrafficLightWithHook } from './02- useEffect/TrafficLightWithHook'
+//import { PokemonPage } from './03- examples/PokemonPage'
+import { FocusScreen } from './04- useRef/FocusScreen'
 
 import './index.css'
 
@@ -12,6 +15,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
      {/* <HookApp /> */}
      {/* <TrafficLight /> */}
-     <TrafficLightWithEffect />
+     {/* <TrafficLightWithEffect /> */}
+     {/* <TrafficLightWithHook /> */}
+     {/* <PokemonPage /> */}
+     <FocusScreen />
   </StrictMode>,
 )
